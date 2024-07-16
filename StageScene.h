0d5f3fd355a7e6ef1e05a27f9ca6c42d3135797f
na <k023g1066@g.neec.ac.jp>
2024-07-16 16:59:02 +0900

    スライドの13までやった

--- StageScene.h
+++ StageScene.h
@@ -4,6 +4,8 @@
 #include"StageSceneInputHandler.h"
 #include"Selector.h"
 #include"IStageSceneCommand.h"
+#include <iostream>
+#include <list>
 
 class StageScene :public IScene
 {
@@ -17,6 +19,10 @@ private:
 	InputHandler* inputHandler_ = nullptr;
 	ICommand* iCommand_ = nullptr;
 	Player* player_;
+
+	StageSceneInputHandler* stageSceneInputHandler_ = nullptr;
+	std::list<IStageSceneCommand*>commandHistory_;
+	std::list<IStageSceneCommand*>::iterator commandHistoryItr_;
 
 	StageSceneInputHandler* stageSceneInputHandler_ = nullptr;
 	Selector* selector_ = nullptr;
