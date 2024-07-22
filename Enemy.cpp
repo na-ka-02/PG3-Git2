816e00952ab459522ea1aa6d2275af347f5fcd6a
na <k023g1066@g.neec.ac.jp>
2024-07-22 15:03:17 +0900

    敵を足した

---
+++ Enemy.cpp
@@ -0,0 +1,16 @@
+#include "Enemy.h"
+
+void Enemy::Init()
+{
+
+}
+
+void Enemy::Update()
+{
+
+}
+
+void Enemy::Draw() const
+{
+
+}
---
+++ Enemy.h
@@ -0,0 +1,10 @@
+#pragma once
+#include"Vector3.h"
+
+class Enemy
+{
+	void Init();
+	void Update();
+	void Draw() const;
+
+};
