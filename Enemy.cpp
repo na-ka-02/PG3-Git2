dbf7892ea7c2ee3ac2369786687e83ead75cb0a1
na <k023g1066@g.neec.ac.jp>
2024-08-02 14:19:06 +0900

    球の処理消した

--- Enemy.cpp
+++ Enemy.cpp
@@ -8,37 +8,13 @@ void Enemy::Init()
 
 void Enemy::Update()
 {
-	if (isEnemyDeadFlag_ == false)
-	{
-		pos_.x += speed_;
-		Return(min, max);
-		Bullet();
-	}
+	pos_.x += speed_;
+	Return(min, max);
 }
 
 void Enemy::Draw() const
 {
 	Novice::DrawBox(int(pos_.x), int(pos_.y), 20, 20, 0.0f, RED, kFillModeSolid);
-}
-
-void Enemy::Bullet()
-{
-	if (bulletFrame_ == 0)
-	{
-		for (int i = 0; i < bulletNum; i++)
-		{
-			if (bulletShotFlag_[i] == false)
-			{
-				bulletShotFlag_[i] = true;
-				bulletPos_[i] = pos_;
-				bulletFrame_ = 20.0f;
-			}
-		}
-	}
-}
-
-void Enemy::EnemyCollition()
-{
 }
 
 Vector2 Enemy::GetPosition()
--- Enemy.h
+++ Enemy.h
@@ -7,24 +7,15 @@ public:
 	void Init();
 	void Update();
 	void Draw() const;
-	void Bullet();
-	void EnemyCollition();
 	Vector2 GetPosition();
 	//跳ね返り処理
 	template<typename position> position Return(position min, position max);
 
 private:
-	const static int bulletNum = 10;
 	Vector2 pos_;
-	Vector2 bulletPos_[bulletNum]{};
-	int deadFrame_ = 120;
 	float speed_ = 3.0f;
-	float bulletSpeed_;
-	float bulletFrame_ = 0.0f;
 	float min = 10;
 	float max = 1270;
-	bool isEnemyDeadFlag_ = false;
-	bool bulletShotFlag_[bulletNum]{};
 };
 
 template<typename position>
